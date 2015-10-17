83f0e9ff4865f04722b6ae68c5ec8d38f3c41430
Michael Gilliland <mjg.py3@gmail.com>
2015-10-17 16:35:52 -0400

    Basic popOver with a button

--- readIt.js
+++ readIt.js
@@ -1,13 +1,36 @@
 (function (document) {
-  var data = {
-    url: document.URL,
-    key: '%KEY%'
+  var sendRead = function () {
+    var data = {
+      url: document.URL,
+      key: '%KEY%'
+    };
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('post', '%HOST%', true);
+    xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+
+    xhr.send(JSON.stringify(data));
   };
 
-  var xhr = new XMLHttpRequest();
-  xhr.open('post', '%HOST%', true);
-  xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+  var createPopover = function () {
+    var div = document.createElement('div');
 
-  xhr.send(JSON.stringify(data));
+    div.style.position = 'fixed';
+    div.style.top = '10px';
+    div.style.left = '50%';
+    div.style.backgroundColor = 'black';
+    div.style.zIndex = 99999999;
+
+    var button = document.createElement('button');
+    button.textContent = 'Read it!';
+    button.onclick = sendRead;
+
+
+    div.appendChild(button);
+    document.body.appendChild(div);
+  };
+
+
+  createPopover();
 
 })(document);
